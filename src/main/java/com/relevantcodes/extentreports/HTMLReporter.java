d1d137e6f756cb14f6b86aecdef98615728814e4
rnhb <Rene.Habermann@t-systems.com>
2017-05-12 10:47:37 +0200

    removed system out log

--- src/main/java/com/relevantcodes/extentreports/HTMLReporter.java
+++ src/main/java/com/relevantcodes/extentreports/HTMLReporter.java
@@ -8,6 +8,19 @@
 
 package com.relevantcodes.extentreports;
 
+import com.relevantcodes.extentreports.model.ExceptionInfo;
+import com.relevantcodes.extentreports.model.Test;
+import com.relevantcodes.extentreports.utils.Resources;
+import com.relevantcodes.extentreports.utils.Writer;
+import com.relevantcodes.extentreports.view.Icon;
+import freemarker.ext.beans.BeansWrapper;
+import freemarker.ext.beans.BeansWrapperBuilder;
+import freemarker.template.Configuration;
+import freemarker.template.Template;
+import freemarker.template.TemplateException;
+import freemarker.template.TemplateHashModel;
+import freemarker.template.TemplateModelException;
+
 import java.io.File;
 import java.io.IOException;
 import java.io.StringWriter;
@@ -21,36 +34,21 @@ import java.util.UUID;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 
-import com.relevantcodes.extentreports.model.ExceptionInfo;
-import com.relevantcodes.extentreports.model.Test;
-import com.relevantcodes.extentreports.utils.Resources;
-import com.relevantcodes.extentreports.utils.Writer;
-import com.relevantcodes.extentreports.view.Icon;
 
-import freemarker.ext.beans.BeansWrapper;
-import freemarker.ext.beans.BeansWrapperBuilder;
-import freemarker.template.Configuration;
-import freemarker.template.Template;
-import freemarker.template.TemplateException;
-import freemarker.template.TemplateHashModel;
-import freemarker.template.TemplateModelException;
-
-
-/**  
+/**
  * Concrete HTMLReporter class
- * 
- * @author Anshoo
  *
+ * @author Anshoo
  */
 public class HTMLReporter extends LogSettings implements IReporter {
     private static final Logger logger = Logger.getLogger(HTMLReporter.class.getName());
-    
+
     private Report report;
-    
+
     private Map<String, Object> templateMap;
-    
+
     private String templateName = "Extent.ftl";
-    
+
     // path of the html file
     private String filePath;
 
@@ -59,54 +57,53 @@ public class HTMLReporter extends LogSettings implements IReporter {
 
     public void start(Report report) {
         this.report = report;
-        
+
         // prevent re-initialization
         if (templateMap != null) {
             return;
         }
 
         ResourceBundle resourceBundle = ResourceBundle.getBundle("com.relevantcodes.extentreports.view.resources.localized", getDocumentLocale());
-        
+
         templateMap = new HashMap<String, Object>();
         templateMap.put("report", this);
         templateMap.put("Icon", new Icon(report.getNetworkMode()));
         templateMap.put("resourceBundle", resourceBundle);
-        
+
         BeansWrapperBuilder builder = new BeansWrapperBuilder(Configuration.VERSION_2_3_23);
         BeansWrapper beansWrapper = builder.build();
-        
+
         try {
-            TemplateHashModel fieldTypeModel = (TemplateHashModel)beansWrapper.getEnumModels().get(LogStatus.class.getName());
+            TemplateHashModel fieldTypeModel = (TemplateHashModel) beansWrapper.getEnumModels().get(LogStatus.class.getName());
             templateMap.put("LogStatus", fieldTypeModel);
-        } 
-        catch (TemplateModelException e) {
+        } catch (TemplateModelException e) {
             e.printStackTrace();
         }
-        
+
         File reportFile = new File(filePath);
 
         if (report.getNetworkMode() == NetworkMode.OFFLINE) {
             templateName = "Extent.Offline.ftl";
-            
+
             initOfflineMode(reportFile);
         }
     }
-    
+
     private void initOfflineMode(File file) {
         String s = "/";
-        
+
         String resourcePackagePath = HTMLReporter.class.getPackage().getName().replace(".", s);
         resourcePackagePath += s + "view" + s;
-        
+
         String cssPath = resourcePackagePath + "offline" + s + "css" + s;
         String fontsPath = cssPath + "fonts" + s;
         String jsPath = resourcePackagePath + "offline" + s + "js" + s;
-        
-        String[] css = { 
+
+        String[] css = {
                 "css.css"
         };
         String[] fonts = {
-                "font-awesome.css.map", 
+                "font-awesome.css.map",
                 "fontawesome-webfont.eot",
                 "fontawesome-webfont.svg",
                 "fontawesome-webfont.ttf",
@@ -134,24 +131,23 @@ public class HTMLReporter extends LogSettings implements IReporter {
                 "Roboto-Thin.woff",
                 "Roboto-Thin.woff2"
         };
-        String[] js = { 
+        String[] js = {
                 "scripts.js"
         };
-        
-        String[] folderNames = { "css" + s + "fonts", "js" };
-        
+
+        String[] folderNames = {"css" + s + "fonts", "js"};
+
         // create offline folders from folderName
         for (String name : folderNames) {
             new File(file.getParent() + s + offlineFolderParent + s + name).mkdirs();
         }
-        
+
         String destPath = file.getParent().replace("\\", s) + s + offlineFolderParent + s;
-        
+
         // copy files to extent/dir
         for (String f : css) {
             String resourcePath = cssPath + f;
             String copyPath = destPath + "css" + s + f;
-            System.out.println("Moving "+resourcePath+" to "+copyPath);
             Resources.moveResource(resourcePath, copyPath);
         }
         for (String f : fonts) {
@@ -161,116 +157,115 @@ public class HTMLReporter extends LogSettings implements IReporter {
             Writer.getInstance().write(new File(destPath + "js" + s + f), Resources.getText(jsPath + f));
         }
     }
-    
+
     public synchronized void flush() {
         String extentSource = null;
-        
+
         try {
             Template template = getConfig().getTemplate(templateName);
-            
+
             StringWriter out = new StringWriter();
-            
+
             try {
                 template.process(templateMap, out);
                 extentSource = out.toString();//.replace("\t", "");
-            } 
-            catch (TemplateException e) {
+            } catch (TemplateException e) {
                 e.printStackTrace();
             }
-            
+
             out.close();
-        }
-        catch (IOException e) {
+        } catch (IOException e) {
             logger.log(Level.SEVERE, "Template not found", e);
         }
 
         Writer.getInstance().write(new File(filePath), extentSource);
     }
-    
+
     private Configuration getConfig() {
         Configuration cfg = new Configuration(Configuration.VERSION_2_3_22);
 
         cfg.setClassForTemplateLoading(HTMLReporter.class, "view");
         cfg.setDefaultEncoding("UTF-8");
-        
+
         return cfg;
     }
-    
+
     public void stop() {
-        
+
     }
-    
+
     public void setTestRunnerLogs() {
 
     }
-    
+
     // adds tests as HTML source
-    public synchronized void addTest(Test test) { }
-    
+    public synchronized void addTest(Test test) {
+    }
+
     public Map<String, String> getConfigurationMap() {
         return report.getConfigurationMap();
     }
-    
+
     public Map<String, List<Test>> getCategoryTestMap() {
         return report.getCategoryTestMap();
     }
-    
+
     public Map<String, List<ExceptionInfo>> getExceptionTestMap() {
         return report.getExceptionTestMap();
     }
-    
+
     public SystemInfo getSystemInfo() {
         return report.getSystemInfo();
     }
-    
+
     public Map<String, String> getSystemInfoMap() {
         return report.getSystemInfoMap();
     }
-    
+
     public List<ExtentTest> getTestList() {
         return report.getTestList();
     }
-    
+
     public Date getStartedTime() {
         return new Date(report.getSuiteTimeInfo().getSuiteStartTimestamp());
     }
-    
+
     public String getRunDuration() {
         return report.getRunDuration();
     }
-    
+
     public String getRunDurationOverall() {
         return report.getRunDurationOverall();
     }
-    
+
     public List<String> getTestRunnerLogList() {
         return report.getTestRunnerLogList();
     }
-    
+
     public List<LogStatus> getLogStatusList() {
         return report.getLogStatusList();
     }
-    
+
     public String getMongoDBObjectID() {
         String id = report.getMongoDBObjectID();
-        
+
         if (id == null) id = "";
-        
+
         return id;
     }
-    
+
     public UUID getReportId() {
         return report.getId();
     }
-    
-    public HTMLReporter(String filePath) { 
+
+    public HTMLReporter(String filePath) {
         this.filePath = filePath;
     }
-    
+
     private Locale getDocumentLocale() {
         return report.getDocumentLocale();
     }
-    
+
     @Deprecated
     public class Config {
         @Deprecated
@@ -282,12 +277,12 @@ public class HTMLReporter extends LogSettings implements IReporter {
         public Config insertCustomStyles(String styles) {
             return this;
         }
-        
+
         @Deprecated
         public Config addCustomStylesheet(String cssFilePath) {
             return this;
         }
-        
+
         @Deprecated
         public Config reportHeadline(String headline) {
             return this;
@@ -303,6 +298,7 @@ public class HTMLReporter extends LogSettings implements IReporter {
             return this;
         }
 
-        public Config() { }
+        public Config() {
+        }
     }
 }
